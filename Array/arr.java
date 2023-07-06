c788e7160ed13964831ab714fbefe5e7c89a8ad4
Aditya Singh <infinity.aditya01@gmail.com>
2023-07-06 21:43:17 +0530

    Adding first Arrays Program in Array file

---
+++ Array/arr.java
@@ -0,0 +1,17 @@
+package Array;
+
+
+class arr {
+    public static void main(String[] args){
+        int[] arr = new int[7];
+        arr[1]=1;
+        arr[2]=2;
+        arr[3]=3;
+        arr[4]=4;
+        arr[5]=5;
+        arr[6]=6;
+        for(int i : arr){
+            System.out.println(i);
+        }
+    }
+}
