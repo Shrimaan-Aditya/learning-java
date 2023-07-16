8e21a5d59b4c42e7d4d67e012057a20e759f2b5f
Aditya Singh <infinity.aditya01@gmail.com>
2023-07-16 11:54:35 +0530

    letarals Represantion Using Other Number System

---
+++ Variables/letaralsRepresantionUsingOtherNumberSys.java
@@ -0,0 +1,14 @@
+package Variables;
+
+class letaralsRepresantionUsingOtherNumberSys {
+    public static void main(String[] args) {
+        int integ1 = 10;
+        int integ2 = 0b1010;
+        int integ3 = 012;
+        int integ4 = 0xA;
+        System.out.println(integ1);
+        System.out.println(integ2);
+        System.out.println(integ3);
+        System.out.println(integ4);
+    }
+}
