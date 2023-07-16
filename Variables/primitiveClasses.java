4faf8c140aa2dfae32cbaf7a3392d119301887eb
Aditya Singh <infinity.aditya01@gmail.com>
2023-07-16 10:58:14 +0530

    Adding Variables directory and a primtive classes program

---
+++ Variables/primitiveClasses.java
@@ -0,0 +1,49 @@
+package Variables;
+
+//import java.lang.*;
+
+class primitiveClasses {
+    public static void main(String[] args) {
+        System.out.println(" Int Data Type");
+        System.out.println("Int Max "+Integer.MAX_VALUE);
+        System.out.println("Int Min "+Integer.MIN_VALUE);
+        System.out.println("Int Bytes "+Integer.BYTES);
+        System.out.println("Int Bits "+Integer.SIZE);
+        System.out.println(" ");
+        System.out.println(" byte Data Type");
+        System.out.println("byte Max "+Byte.MAX_VALUE);
+        System.out.println("byte Min "+Byte.MIN_VALUE);
+        System.out.println("byte Bytes "+Byte.BYTES);
+        System.out.println("byte Bits "+Byte.SIZE);
+        System.out.println(" ");
+        System.out.println(" Short Data Type");
+        System.out.println("Short Max "+Short.MAX_VALUE);
+        System.out.println("Short Min "+Short.MIN_VALUE);
+        System.out.println("Short Bytes "+Short.BYTES);
+        System.out.println("Short Bits "+Short.SIZE);
+        System.out.println(" ");
+        System.out.println(" Float Data Type");
+        System.out.println("Float Max "+Float.MAX_VALUE);
+        System.out.println("Float Min "+Float.MIN_VALUE);
+        System.out.println("Float Bytes "+Float.BYTES);
+        System.out.println("Float Bits "+Float.SIZE);
+        System.out.println(" ");
+        System.out.println(" Double Data Type");
+        System.out.println("Double Max "+Double.MAX_VALUE);
+        System.out.println("Double Min "+Double.MIN_VALUE);
+        System.out.println("Double Bytes "+Double.BYTES);
+        System.out.println("Double Bits "+Double.SIZE);
+        System.out.println(" ");
+        System.out.println(" Char Data Type");
+        System.out.println("Char Max "+Character.MAX_VALUE);
+        System.out.println("Char Min "+Character.MIN_VALUE);
+        System.out.println("Char Bytes "+Character.BYTES);
+        System.out.println("Char Bits "+Character.SIZE);
+        System.out.println(" ");
+        System.out.println(" Bool Data Type");
+        System.out.println("Bool Max "+Boolean.TRUE);
+        System.out.println("Bool Min "+Boolean.FALSE);
+        System.out.println("Bool Bytes "+Boolean.TYPE);
+        System.out.println(" ");
+    }
+}
