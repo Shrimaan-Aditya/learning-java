34dbc41b48ab4d98843d963f6dfbef7ebc27fd54
Aditya Singh <infinity.aditya01@gmail.com>
2023-07-17 13:49:31 +0530

    using unicode in java

---
+++ General/uniCodeOfHindi.java
@@ -0,0 +1,11 @@
+package General;
+
+class uniCordOfHindi{
+    public static void main(String[] args) {
+        // for(char i=0x0900;i<=0x0970;i++){
+        //     System.out.print(i+" ");
+        // }
+        char a = 0x0900;
+        System.out.println(a);
+    }
+}
